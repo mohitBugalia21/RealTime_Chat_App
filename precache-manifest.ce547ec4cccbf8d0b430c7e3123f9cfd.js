3795cad4e68c67ac1d59f5d8d68643a3c757d8e8
MOHIT BUGALIA <mohitbugalia921@gmail.com>
2025-07-31 20:24:35 +0530

    Updates

---
+++ precache-manifest.ce547ec4cccbf8d0b430c7e3123f9cfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf68f00e6730653bd6ea947340b3624b",
+    "url": "/RealTime_Chat_App/index.html"
+  },
+  {
+    "revision": "499199dee5c2b62c4a54",
+    "url": "/RealTime_Chat_App/static/css/main.2ef03f7d.chunk.css"
+  },
+  {
+    "revision": "dfd3980291314fb8c4b3",
+    "url": "/RealTime_Chat_App/static/js/2.40623496.chunk.js"
+  },
+  {
+    "revision": "499199dee5c2b62c4a54",
+    "url": "/RealTime_Chat_App/static/js/main.49409681.chunk.js"
+  },
+  {
+    "revision": "a7d045cdf81cfcfc2721",
+    "url": "/RealTime_Chat_App/static/js/runtime~main.0bb44224.js"
+  }
+]);
